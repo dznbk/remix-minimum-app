967a18fbc788b292d074186cbd82b73bcd748698
DPontaro <n.douzono@gmail.com>
2025-01-04 16:04:38 +0900

    ts settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+import esbuild from "esbuild";
 
 export default defineConfig({
   plugins: [
@@ -15,9 +10,24 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
       },
+      serverBuildFile: 'remix.js',
+      buildEnd: async () => {
+        await esbuild.build({
+          alias: { "~": "./app" },
+          outfile: "build/server/index.js",
+          entryPoints: ["server/index.ts"],
+          external: ['./build/server/*'],
+          platform: 'node',
+          format: 'esm',
+          packages: 'external',
+          bundle: true,
+          logLevel: 'info',
+        }).catch((error: unknown) => {
+          console.error('Error building server:', error);
+          process.exit(1);
+        });
+      }
     }),
     tsconfigPaths(),
   ],
